2809dae5ecc9f3dbd5f35b79e95b81768c56085e
innovationwizard <36010111+innovationwizard@users.noreply.github.com>
2025-05-27 08:35:57 -0600

    Adding in render env the env variables from railway.

--- app.py
+++ app.py
@@ -38,11 +38,11 @@ if DATABASE_URL:
     }
 else:
     DB_CONFIG = {
-        'host': os.getenv("postgres.railway.internal"),
-        'user': os.getenv("postgres"),
-        'password': os.getenv("AfzCdlJXGrviGQPnAEMmSOhuNFrCNHke"),
-        'dbname': os.getenv("railway"),
-        'port': os.getenv("5432"),
+        'host': os.getenv("DB_HOST"),
+        'port': os.getenv("DB_PORT", "5432"),
+        'user': os.getenv("DB_USER"),
+        'password': os.getenv("DB_PASSWORD"),
+        'dbname': os.getenv("DB_NAME"),
         'sslmode': 'require'
     }
     if not all([DB_CONFIG['host'], DB_CONFIG['user'], DB_CONFIG['password'], DB_CONFIG['dbname']]):
